39ded6335503ff38569e740b73be7295c50317d3
zerom0 <github@martinmosler.de>
2016-11-22 08:00:46 +0100

    Added unittests for NOT resources.

---
+++ tests/NotTests.cpp
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "gtest/gtest.h"
+
+#include "Resources/Not.h"
+#include "Resources/Resource.h"
+
+TEST(Not, true) {
+  auto resource = notResourceFactory();
+  resource->setProperty("inputValue", "true");
+  ASSERT_EQ("false", resource->readProperty("value"));
+}
+
+TEST(Not, false) {
+  auto resource = notResourceFactory();
+  resource->setProperty("inputValue", "false");
+  ASSERT_EQ("true", resource->readProperty("value"));
+}
